dc49d3efc3caa0557605dec476af5b09b9c624af
Ed Tervit <edtervit@hotmail.co.uk>
2021-04-25 22:37:53 +0100

    basic for past 20

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,85 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
 
-export default function Home() {
+export default function Home(props) {
+  console.log(props);
+
   return (
-    <div className={styles.container}>
+    <div className="flex text-center flex-wrap">
       <Head>
-        <title>Create Next App</title>
+        <title>Warzone Stat Tracker</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <h1 className="w-full my-4 text-4xl">Past 20 Games</h1>
+      {props.twenty.map((player, index) => (
+        <div className="w-1/3">
+          <h1 className="text-2xl">{player.name}</h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+          <h2 className="mt-4 font-bold">Gulag</h2>
+          <p>
+            {player.summary.all.gulagKills} kills /{" "}
+            {player.summary.all.gulagDeaths} deaths
+          </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <h2 className="mt-4 font-bold">Ingame</h2>
+          <p>
+            {player.summary.all.kills} kills / {player.summary.all.deaths}{" "}
+            deaths
+          </p>
+          <p>{player.summary.all.kdRatio} ratio</p>
+          <p>Damage done: {player.summary.all.damageDone}</p>
+          <p></p>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      ))}
     </div>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  const res = await fetch(
+    "https://call-of-duty-modern-warfare.p.rapidapi.com/warzone-matches/uMad%20Tervit/xbl",
+    {
+      method: "GET",
+      headers: {
+        "x-rapidapi-key": "30a268d3a6mshee5a67ac97e807cp183134jsn72a3dfdf6e2e",
+        "x-rapidapi-host": "call-of-duty-modern-warfare.p.rapidapi.com",
+      },
+    }
+  );
+  const ed = await res.json();
+
+  await new Promise((r) => setTimeout(r, 1500));
+
+  const resAz = await fetch(
+    "https://call-of-duty-modern-warfare.p.rapidapi.com/warzone-matches/rehsto/xbl",
+    {
+      method: "GET",
+      headers: {
+        "x-rapidapi-key": "30a268d3a6mshee5a67ac97e807cp183134jsn72a3dfdf6e2e",
+        "x-rapidapi-host": "call-of-duty-modern-warfare.p.rapidapi.com",
+      },
+    }
+  );
+  const az = await resAz.json();
+
+  await new Promise((r) => setTimeout(r, 1500));
+
+  const resshaun = await fetch(
+    "https://call-of-duty-modern-warfare.p.rapidapi.com/warzone-matches/uMad%20Bean/xbl",
+    {
+      method: "GET",
+      headers: {
+        "x-rapidapi-key": "30a268d3a6mshee5a67ac97e807cp183134jsn72a3dfdf6e2e",
+        "x-rapidapi-host": "call-of-duty-modern-warfare.p.rapidapi.com",
+      },
+    }
+  );
+  const shaun = await resshaun.json();
+  shaun.name = "Shaun";
+  ed.name = "Ed";
+  az.name = "Az";
+
+  const twenty = [az, ed, shaun];
+
+  return { props: { twenty }, revalidate: 60 };
 }
