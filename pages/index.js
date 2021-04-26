1354e469e5ad4d4805d75da5eb46b0d0fb372021
Ed Tervit <edtervit@hotmail.co.uk>
2021-04-26 11:21:43 +0100

    added all time stats

--- pages/index.js
+++ pages/index.js
@@ -31,6 +31,25 @@ export default function Home(props) {
           <p></p>
         </div>
       ))}
+
+      <h2 className="w-full my-4 mt-8 text-4xl">All Time</h2>
+      {props.all.map((player, index) => (
+        <div className="w-1/3">
+          <h3 className="text-2xl">{player.name}</h3>
+
+          <h2 className="mt-4 font-bold">Wins</h2>
+          <p>
+            {player.br.wins} wins / {player.br.gamesPlayed} games
+          </p>
+
+          <h2 className="mt-4 font-bold">Ingame</h2>
+          <p>
+            {player.br.kills} kills / {player.br.deaths} deaths
+          </p>
+          <p>{player.br.kdRatio} ratio</p>
+          <p></p>
+        </div>
+      ))}
     </div>
   );
 }
@@ -46,7 +65,7 @@ export async function getStaticProps() {
       },
     }
   );
-  const ed = await res.json();
+  const edTwenty = await res.json();
 
   await new Promise((r) => setTimeout(r, 1500));
 
@@ -60,7 +79,7 @@ export async function getStaticProps() {
       },
     }
   );
-  const az = await resAz.json();
+  const azTwenty = await resAz.json();
 
   await new Promise((r) => setTimeout(r, 1500));
 
@@ -74,12 +93,57 @@ export async function getStaticProps() {
       },
     }
   );
-  const shaun = await resshaun.json();
-  shaun.name = "Shaun";
-  ed.name = "Ed";
-  az.name = "Az";
+  const shaunTwenty = await resshaun.json();
+  shaunTwenty.name = "Shaun";
+  edTwenty.name = "Ed";
+  azTwenty.name = "Az";
 
-  const twenty = [az, ed, shaun];
+  const twenty = [azTwenty, edTwenty, shaunTwenty];
 
-  return { props: { twenty }, revalidate: 60 };
+  const resEdAll = await fetch(
+    "https://call-of-duty-modern-warfare.p.rapidapi.com/warzone/uMad%20Tervit/xbl",
+    {
+      method: "GET",
+      headers: {
+        "x-rapidapi-key": "30a268d3a6mshee5a67ac97e807cp183134jsn72a3dfdf6e2e",
+        "x-rapidapi-host": "call-of-duty-modern-warfare.p.rapidapi.com",
+      },
+    }
+  );
+  const edAll = await resEdAll.json();
+
+  await new Promise((r) => setTimeout(r, 1500));
+
+  const resAzAll = await fetch(
+    "https://call-of-duty-modern-warfare.p.rapidapi.com/warzone/rehsto/xbl",
+    {
+      method: "GET",
+      headers: {
+        "x-rapidapi-key": "30a268d3a6mshee5a67ac97e807cp183134jsn72a3dfdf6e2e",
+        "x-rapidapi-host": "call-of-duty-modern-warfare.p.rapidapi.com",
+      },
+    }
+  );
+  const azAll = await resAzAll.json();
+
+  await new Promise((r) => setTimeout(r, 1500));
+
+  const resshaunAll = await fetch(
+    "https://call-of-duty-modern-warfare.p.rapidapi.com/warzone/uMad%20Bean/xbl",
+    {
+      method: "GET",
+      headers: {
+        "x-rapidapi-key": "30a268d3a6mshee5a67ac97e807cp183134jsn72a3dfdf6e2e",
+        "x-rapidapi-host": "call-of-duty-modern-warfare.p.rapidapi.com",
+      },
+    }
+  );
+  const shaunAll = await resshaunAll.json();
+  shaunAll.name = "Shaun";
+  edAll.name = "Ed";
+  azAll.name = "Az";
+
+  const all = [azAll, edAll, shaunAll];
+
+  return { props: { twenty, all }, revalidate: 60 };
 }
